b78d01577f305c0e08d91a456771873a45536dfe
joelbobai <90658305+joelbobai@users.noreply.github.com>
2023-08-10 02:28:04 +0100

    Update auth.js

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -3,7 +3,13 @@ const jwt = require("jsonwebtoken");
 const { JWT_SECRET } = process.env;
 
 const verifyToken = async (req, res, next) => {
-  let cookies = req.headers.cookie;
+  let cookies;
+  if(req.headers.cookie){
+    cookies = req.headers.cookie
+  }else if(req.header("Authorization")){
+    cookies = req.header("Authorization");
+  }
+
   if (!cookies) {
     return res.status(403).send("Access Denied, Token is required!");
   }
